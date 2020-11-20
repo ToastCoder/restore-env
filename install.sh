2451e648db6407645e8f61a28b296eff7fd252c2
ToastCoder <vicky.pcbasic@gmail.com>
2020-11-20 23:38:39 +0530

    Added vscode_deb()

--- install.sh
+++ install.sh
@@ -66,6 +66,26 @@ git_deb()
     fi
 }
 
+# SUB FUNCTION TO INSTALL VSCODE (DEBIAN)
+vscode_deb()
+{
+    echo "Checking if Visual Studio Code is already installed..."
+    if which code >/dev/null;
+    then
+        echo "Visual Studio Code is already installed."
+        echo "Skipping Visual Studio Code installation..."
+    else
+        echo "Installing Visual Studio Code..."
+        wget -qO- https://packages.microsoft.com/keys/microsoft.asc | gpg --dearmor > packages.microsoft.gpg
+        sudo install -o root -g root -m 644 packages.microsoft.gpg /etc/apt/trusted.gpg.d/
+        sudo sh -c 'echo "deb [arch=amd64 signed-by=/etc/apt/trusted.gpg.d/packages.microsoft.gpg] https://packages.microsoft.com/repos/vscode stable main" > /etc/apt/sources.list.d/vscode.list'
+        sudo apt-get install apt-transport-https
+        sudo apt-get update
+        sudo apt-get install code
+        echo "Successfully installed Visual Studio Code"
+    fi
+}
+
 # FUNCTION TO INSTALL APPS ON DEBIAN BASED DISTROS
 install_deb()
 {
